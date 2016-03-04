69e1a2c6af2fa98d8cf795bd79089f9727c8578a
kjiwu <kjiwu@hotmail.com>
2016-03-04 11:46:29 +0800

    add helper

---
+++ TrafficRulesExam/TrafficRulesExam/Helper/UserDataHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace TrafficRulesExam.Helper
+{
+    public class UserDataHelper
+    {
+        class UserDataKey
+        {
+            public static string Subject1SequenceIdKey
+            {
+                get { return "Subject1SequenceIdKey"; }
+            } 
+        }
+
+        public static void Save(string key, object value)
+        {
+            ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values[key] = value;
+        }
+
+        public static object GetValue(string key)
+        {
+            ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            return localSettings.Values[key];
+        }
+    }
+}
